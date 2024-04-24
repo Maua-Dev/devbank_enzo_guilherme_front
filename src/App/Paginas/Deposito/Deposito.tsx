d3f9477c957b8a6d1d1b7b274b4a992acd2d6e49
Pistori1 <enzopistori71@gmail.com>
2024-04-24 11:24:36 -0300

    Deposit Page usage text

--- src/App/Paginas/Deposito/Deposito.tsx
+++ src/App/Paginas/Deposito/Deposito.tsx
@@ -13,6 +13,11 @@ export default function DepositPage() {
         <p className="top_text left">Saldo atual: R$ {credito}</p>
         <p className="top_text right">Quantidade sacada: R$ </p>
       </div>
+      <div className="usage_position">
+        <h1 className="usage_explanation">
+          Selecione as cédulas e a quantidade que você deseja
+        </h1>
+      </div>
     </main>
   );
 }
