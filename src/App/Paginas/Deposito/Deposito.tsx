58ec29d6706185d95a7f3aea632b7926daf9c637
Pistori1 <enzopistori71@gmail.com>
2024-04-23 23:37:27 -0300

    top text deposit screen

--- src/App/Paginas/Deposito/Deposito.tsx
+++ src/App/Paginas/Deposito/Deposito.tsx
@@ -4,24 +4,15 @@ import { ApiContext } from "../../Context/api-context";
 import HeaderGeral from "../Interface/headergeral";
 
 export default function DepositPage() {
-    const {
-      name,
-      account,
-      agency,
-      credito
-    } = useContext(ApiContext);
-  
-  
-  
-    return (
-      <main className="deposit_page">
-        <HeaderGeral name={name} account={account} agency={agency}/>
-        <div className="deposit_top_box">
-          <div className="left_box">
-            <p className="left_text">Saldo atual: R$ {credito}</p>
-          </div>
-          <p className="deposit_right_text">Quantidade sacada: R$ </p>
-        </div>
-      </main>
-    );
+  const { name, account, agency, credito } = useContext(ApiContext);
+
+  return (
+    <main className="deposit_page">
+      <HeaderGeral name={name} account={account} agency={agency} />
+      <div className="deposit_top_box">
+        <p className="top_text left">Saldo atual: R$ {credito}</p>
+        <p className="top_text right">Quantidade sacada: R$ </p>
+      </div>
+    </main>
+  );
 }
