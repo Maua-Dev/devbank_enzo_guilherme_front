4c3cf9a4ad24be3f6cbc50d3b6696f20a3f3a864
edkod <23.01612-4@maua.br>
2024-04-28 19:46:38 -0300

    DEVBANK3

--- src/App/AppRouter.tsx
+++ src/App/AppRouter.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PaginaPrincipal from "../App/Paginas/PaginaPrincial/PaginaPrincipal";
 import PaginaConta from "./Paginas/PaginaConta/PaginaConta";
-import Deposito from "./Paginas/Deposito/Deposito"; 
-// import Retirada from "./Paginas/PaginaConta/PaginaConta"; 
+import Deposito from "./Paginas/Deposito/Deposito";
+import Transações from "./Paginas/Transações/Transações"; 
 
 export default function AppRouter() {
     return (
@@ -11,6 +11,7 @@ export default function AppRouter() {
                 <Route path="/" element={<PaginaPrincipal />} />
                 <Route path="/PaginaConta" element={<PaginaConta />}/>
                 <Route path="/PaginaConta/Deposito" element={<Deposito />}/>
+                <Route path="/PaginaConta/Transações" element={<Transações />}/>
             </Routes>
         </BrowserRouter>
     )
